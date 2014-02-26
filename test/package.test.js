920058a2c9ae3cab00983f475f51e874bcbdefa0
Jared Hanson <jaredhanson@gmail.com>
2014-02-26 14:07:28 -0800

    Initial commit.

---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('passport-ssl-certificate', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
